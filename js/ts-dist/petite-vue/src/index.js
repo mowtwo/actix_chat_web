6ba1a1e1d4268f75ddd4a5099ef66e918173fd16
mowtwo <1046099511@qq.com>
2022-02-11 17:21:02 +0800

    删除不必要的代码

--- js/ts-dist/petite-vue/src/index.js
+++
@@ -1,8 +0,0 @@
-export { createApp } from './app';
-export { nextTick } from './scheduler';
-export { reactive } from '@vue/reactivity';
-import { createApp } from './app';
-var s = document.currentScript;
-if (s && s.hasAttribute('init')) {
-    createApp().mount();
-}
--- js/ts-dist/petite-vue/src/utils.js
+++
@@ -1,9 +0,0 @@
-export var checkAttr = function (el, name) {
-    var val = el.getAttribute(name);
-    if (val != null)
-        el.removeAttribute(name);
-    return val;
-};
-export var listen = function (el, event, handler, options) {
-    el.addEventListener(event, handler, options);
-};
